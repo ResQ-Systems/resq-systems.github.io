e46c94866fd30ead344a870f1d09ab20db39a072
Oscar Morris <twocap06@gmail.com>
2025-04-24 23:34:07 +0100

    mobile version?

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
-function emailSubmit() {
-    return false;
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleNav() {
+  var x = document.getElementById("mobile-nav");
+  if (x.className === "mobile-nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "mobile-nav";
+  }
 }

d725eadddf0b47999f4744e608171d152a618a69
AdamM <adam@mathieson.dev>
2025-04-25 02:20:31 +0100

    + Start of website tidy up and new animations

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function toggleNav() {
-  var x = document.getElementById("mobile-nav");
+  const x = document.getElementById("mobile-nav");
   if (x.className === "mobile-nav") {
     x.className += " responsive";
   } else {

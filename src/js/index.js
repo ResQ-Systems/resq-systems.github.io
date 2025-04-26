08a6fed908f4077323eb4a04316407e74405506b
AdamM <adam@mathieson.dev>
2025-04-26 01:44:04 +0100

    + MAJOR restructure  + Initial support of vite & SASS  + Pipeline for deployment  + Refactor colours and brand details to scss file  + Header & footer managed by custom elements now

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+import "../css/index.scss"
+import footer_comp from "./components/footer.js";
+import header_comp from "./components/header.js";
+
+[footer_comp, header_comp].forEach((cmp) => {
+  customElements.define(cmp.name, cmp.component);
+});
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleNav() {
+  const x = document.getElementById("mobile-nav");
+  if (x.className === "mobile-nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "mobile-nav";
+  }
+}
+document.getElementById("nav-button").addEventListener("click", toggleNav);
+document.querySelectorAll(".mobile-nav").forEach((item) => {
+  item.addEventListener("click", toggleNav);
+})
